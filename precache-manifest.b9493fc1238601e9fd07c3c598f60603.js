bbbfa3884021115a532e155fd82ac47d4cc55b95
yahve0110 <ilik124darc@gmail.com>
2024-10-30 14:54:06 +0200

    Updates

---
+++ precache-manifest.b9493fc1238601e9fd07c3c598f60603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f3ebd080600c88e9fb390b8df6bc9d",
+    "url": "/graphE/index.html"
+  },
+  {
+    "revision": "cbbf321866d15bb2877d",
+    "url": "/graphE/static/css/main.78b12424.chunk.css"
+  },
+  {
+    "revision": "b1fe568d429995c90e7b",
+    "url": "/graphE/static/js/2.98665a28.chunk.js"
+  },
+  {
+    "revision": "cbbf321866d15bb2877d",
+    "url": "/graphE/static/js/main.5745eea7.chunk.js"
+  },
+  {
+    "revision": "135ed552948221228774",
+    "url": "/graphE/static/js/runtime~main.64925a34.js"
+  }
+]);
